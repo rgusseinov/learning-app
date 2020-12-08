f3146d881f06dd76433facf2fa8d0133a3efd2a5
Ruslan Gusseinov <ruslan.gcnto@yandex.ru>
2020-12-08 23:56:16 +0600

    Added login form

--- src/components/login.js
+++ src/components/login.js
@@ -1,12 +1,34 @@
+import { loginPage } from "./templates"
+
 class Login {
   constructor(selector){
     this.$el = document.querySelector(selector)
   }
 
   render(){
+    let html = loginPage()
+
     this.$el.innerHTML = ''
-    this.$el.insertAdjacentHTML('beforeend', `<h1> Welcome to Login page </h1>`)
+    this.$el.insertAdjacentHTML('beforeend', html)
+    this.iniListeners()
+
   }
+
+  iniListeners(){
+    document.querySelector('#loginForm').addEventListener('submit', function(e){
+      e.preventDefault()
+      let firstName = this.querySelector('[name="first_name"]').value
+      let lastName = this.querySelector('[name="last_name"]').value
+
+      let formData = {
+        firstName, 
+        lastName
+      }
+      console.log(`formData`, formData)
+    })
+  }
+
+
   
 }
 
--- src/components/templates.js
+++ src/components/templates.js
@@ -1,5 +1,31 @@
-function renderCurrency(name, rate){
+export function renderCurrency(name, rate){
   return `<a href="#!" class="collection-item"><span class="badge">${name}</span>${rate}</a>`
 }
 
-export default renderCurrency
+
+export function loginPage(){
+  return `
+    <div class="row">
+      <form class="col s12" id="loginForm">
+        <div class="row">
+            <div class="input-field col s6">
+              <input placeholder="Placeholder" id="first_name" name="first_name" type="text" class="validate">
+              <label for="first_name">First Name</label>
+            </div>
+            <div class="input-field col s6">
+              <input id="last_name" type="text" name="last_name" class="validate">
+              <label for="last_name">Last Name</label>
+            </div>
+          </div> 
+
+          <div class="row">
+            <div class="input-field col s6">
+              <button class="btn waves-effect waves-light" type="submit" name="action"> Submit </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+        
+  `
+}
