93a3cff035381cd2154656d61e29e3b6b8d76f23
Ruslan Gusseinov <ruslan.gcnto@yandex.ru>
2020-12-17 17:22:32 +0600

    Prepared for trello project

--- src/components/api.js
+++
@@ -1,7 +0,0 @@
-async function getExchangeRates(){ 
-  const URL = 'http://localhost:3001/rates'
-  const response = await fetch(URL)
-  return await response.json()
-}
-
-export default getExchangeRates
--- src/components/utils.js
+++
@@ -1,7 +0,0 @@
-export function getStorage(){
-  return JSON.parse(localStorage.getItem('tasks')) || []
-}
-
-export function setStorage(name, options) {
-  localStorage.setItem(name, JSON.stringify(options))
-}
