baf9b578a54930fb3814d754f51f46281aa78a13
Ruslan Gusseinov <ruslan.gcnto@yandex.ru>
2021-01-07 21:25:38 +0600

    Prepared project

--- src/components/Templates.js
+++
@@ -1,6 +0,0 @@
-export function getEditView(name){
-  return `
-    <input class="input quickSave" type="text" value="${name}">
-    <span class="icon has-text-info actionSaveItem"><i class="fas fa-save"></i></span>  
-  `
-}
--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
 import Header from './components/Header';
 import Blocks from './components/Blocks'
 import Main from './components/Main'
-import './style.css';
+// import './style.css';
 
-
-/*
-  - Создание карточки + 
-  - Удаление карточки
-  - Добавление/удаление/редактирование задачи + 
-  - Перемещение карточки +
-  - Перемещение задач между карточками
-  - Редактирование название блока
-  - Просто перемещение одной задачи из одной карточки в другую
-  - Удаление блока
-  
-  --- Эффекты
-    
-
-  Сущности
-  
-  - Карточки
-  - Drag and Drop
-
-  */
 
 const main = new Main('#app', {
   components: [Blocks]}
