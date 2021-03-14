45f35cd85f23562b956dd362f6a70be229435df3
Ruslan Gusseinov <ruslan.gcnto@yandex.ru>
2021-03-14 17:03:08 +0600

    Singleton

--- src/components/Storage.js
+++
@@ -1,7 +0,0 @@
-export function getStorage(){
-  return JSON.parse(localStorage.getItem('blocks')) || []
-}
-
-export function setStorage(data){
-  localStorage.setItem('blocks', JSON.stringify(data))
-}
